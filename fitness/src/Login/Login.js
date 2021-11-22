219386241d71cf1756381bbbe19b4e66da8cdcf8
Solguden <55831381+Solguden@users.noreply.github.com>
2021-11-22 23:02:05 +0100

    initial login

--- fitness/src/Login/Login.js
+++ fitness/src/Login/Login.js
@@ -1,7 +1,84 @@
+import { useState } from "react";
+import { handleResponse } from './handle-response';
+import { authHeader } from './auth-header'
+
+export function logout(){
+  localStorage.removeItem('currentUser');
+}
+
 export function Login () {
-    return (
-      <main>
-        <p>Login!</p>
-      </main>
-    );
+  const initialState = { email: '', password: '' };
+  const [state, setState] = useState(initialState);
+
+  function handleChangeEmail(event) {
+    console.log(event.target.value)
+    setState({ email: event.target.value });
   }
+
+  function handleChangePassword(event) {
+    console.log(event.target.value)
+    setState({ password: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    const email = state.email
+    const password = state.password
+    event.preventDefault();
+    
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    };
+    return fetch(`https://afe2021fitness.azurewebsites.net/api/Users/login`, requestOptions)
+      // .then(resp => resp.json())
+      .then(handleResponse)
+      .then(user => {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          console.log(user)
+          return user;
+      });
+  }    
+
+  function getAll() {
+    const requestOptions = { method: 'GET', headers: authHeader() };
+    return fetch(`https://afe2021fitness.azurewebsites.net/api/Users`, requestOptions).then(handleResponse);
+  }
+
+  function setManager(){
+    setState({ email: "boss@fitness.moon", password: "asdfQWEaQR" });
+  }
+
+  function setTrainer(){
+    setState({ email: "m@fit", password: "aQ" });
+  }
+  function setClient(){
+    setState({ email: "c1@fit", password: "aA" });
+  }
+
+  return (
+    <main>
+      <p>Login as:</p>
+      <button onClick={setManager}>Manager</button>
+      <button onClick={setClient}>Trainer</button>
+      <button onClick={setTrainer}>Client</button>
+
+      <form onSubmit={handleSubmit}>
+          <label>
+            Email:
+            <input type="email" value={state.email} onChange={handleChangeEmail}></input>
+          </label>
+          <br/>
+          <label>
+            Password:
+            <input type="password" value={state.password} onChange={handleChangePassword}></input>
+          </label>
+          <br/>
+          <input type="submit" value="Login"></input>
+      </form>
+      <button onClick={getAll}>Test</button>
+      <button onClick={logout}>Logout</button>
+    </main>
+  );
+}
---
+++ fitness/src/Login/loginForm.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+export function loginForm () {
+    return (
+        <main>
+        
+        </main>
+    );
+}
