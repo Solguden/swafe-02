1ce8c54f66f6459dfef817644c03679385cd8d3c
JSvoldgaard <Jacob.svoldgaard93@gmail.com>
2021-11-22 23:04:00 +0100

    First iteration of Trainer.

---
+++ fitness/src/util/api.js
@@ -0,0 +1,12 @@
+export function getData(url) {
+
+    return fetch(url)
+      .then(resp => {
+  
+        if (!resp.ok) {
+          throw Error("There was a problem fetching data.");
+        }
+  
+        return resp.json();
+      });
+  }
